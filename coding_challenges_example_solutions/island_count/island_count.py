2a5475dae562c5151ba45cbf5cbb9d68ad360329
mark.broich@gmail.com <mark.broich@gmail.com>
2021-02-20 14:50:25 -0500

    X cluster count [island count revisted]

--- coding_challenges_example_solutions/island_count/island_count.py
+++ coding_challenges_example_solutions/island_count/island_count.py
@@ -19,14 +19,15 @@
 #         [0, 1, 0]]
 # the correct answer here 3 islands
 
-# time O(columes * rows)
+# time O(columns * rows)
 # explantation: time O() is proportional to the total graph's number of visited nodes and edges. 
-# In that case, there are columes * rows nodes and slightly less than 4 * columes * rows edges, their sum is still O(columes * rows).
+# In that case, there are columns * rows nodes and slightly less than 4 * columns * rows edges, their sum is still O(columns * rows).
+# so O(n)
 
 # Why? b/c we call each edge exactly once in each direction (even if the call returns right away)
 
 # space O(1) + call stack which is up to 
-# columes * rows in the worth case 
+# columns * rows in the worth case 
 
 def count_islands(grid):
     counter = 0
@@ -107,3 +108,86 @@ grid = [[0, 0, 0],
 
 print(count_islands(grid), " islands found")
 print("")
+
+
+# solved again w slightly different approach. 
+
+# just to recall: 
+# time O(columns * rows)
+# explantation: time O() is proportional to the total graph's number of visited nodes and edges. 
+# In that case, there are columns * rows nodes and slightly less than 4 * columns * rows edges, their sum is still O(columns * rows).
+# so O(n)
+
+## X cluster counter
+# 
+# given an array with X and O as e.g. below:
+# find the number of Xs that are either part 
+# of a cluster or on their own. 
+#
+# [["O","O","O","X","O","O","O"]
+#  ["O","X","O","O","O","X","O"], 
+#  ["O","X","O","O","O","X","O"]]
+
+# A cluster is defined as adjacent Xs in 
+# horizontal or vertical direction
+# own. Clusters w multiple members are 
+# flagged for illustation 
+# w '.' below. 
+
+# [["O","O", "O", "X","O","O", "O"]
+#  ["O","O", "O", "O","O","X.","O"], 
+#  ["O","X.","X.","O","O","X.","O"]]
+
+# the counter of all clusters in this example is: 3
+
+class XClusterCounter:
+	# @param A : list of strings
+	# @return an integer
+    def __init__(self):
+        self._myDict = {}
+        self._counter = 0
+
+    def __explore_adj(self, A, i, j):
+        # flag as visited
+        self._myDict[(i,j)] = "T"
+        #
+        # left
+        if j-1 >= 0: 
+            if A[i][j-1] =='X' and not (i,j-1) in self._myDict:
+                self.__explore_adj(A, i,j-1)
+        # right
+        if j+1 <= (len(A[i])-1):
+            if A[i][j+1] =='X' and not (i,j+1) in self._myDict:
+                self.__explore_adj(A, i,j+1)
+        # above
+        if i-1 >= 0:
+            if A[i-1][j] =='X' and not (i-1,j) in self._myDict:
+                self.__explore_adj(A, i-1,j)
+        # below
+        if i+1 <= len(A)-1:
+            if A[i+1][j] =='X' and not (i+1,j) in self._myDict: 
+                    self.__explore_adj(A, i+1,j)
+
+    def counter(self,A):
+        #
+        # loop over each list and each cell within list
+        for i in range(0,len(A)):
+                for j in range(0,len(A[i])):
+                    if A[i][j] == 'X' and not (i,j) in self._myDict:
+                        # new island found
+                        self._counter += 1
+                        # look for adjacent
+                        self.__explore_adj(A, i, j)
+        return self._counter
+        
+
+
+        
+# ["O","O","O","X","O","O","O"]
+# ["O","X","O","O","O","X","O"], 
+# ["O","X","O","O","O","X","O"] 
+
+A = [ ["O","O","O","X","O","O","O"], ["O","X","O","O","O","X","O"], ["O","X","O","O","O","X","O"] ]
+XC1 = XClusterCounter()
+print(XC1.counter(A))
+
