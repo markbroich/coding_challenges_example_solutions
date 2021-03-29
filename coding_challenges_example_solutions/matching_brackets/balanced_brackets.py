55989c618e1ae469ce032733f9353ef06683103d
mark.broich@gmail.com <mark.broich@gmail.com>
2021-03-29 10:42:02 -0400

    added bracket mismatch count

--- coding_challenges_example_solutions/matching_brackets/balanced_brackets.py
+++ coding_challenges_example_solutions/matching_brackets/balanced_brackets.py
@@ -51,3 +51,73 @@ print(balanced_brackets(string))
 string = None
 #Output: -1 
 print(balanced_brackets(string))
+
+
+#### a related question:
+# return the number of un match brackets
+
+
+# the string can only consist of 
+# combinations of '(' and ')'
+
+# using a stack
+# Ot(n) 
+# Os(n) if all '(' or all ')'
+def bracket_match(string):
+    stack = []
+    for i in range(0,len(string)):
+        if string[i] == '(':
+            stack.append('(')
+        elif len(stack) > 0:
+            if stack[-1] == '(':
+                stack.pop()
+            else: 
+                stack.append(')')
+        else: 
+            stack.append(')')
+    return len(stack)
+
+def tests():
+    string = ')))()('
+    print(bracket_match(string) == 4)
+    string = ')'
+    print(bracket_match(string) == 1)
+    string = '('
+    print(bracket_match(string) == 1)
+    string = '(())'
+    print(bracket_match(string) == 0)
+    string = '(()'
+    print(bracket_match(string) == 1)
+    string = '())('
+    print(bracket_match(string) == 2)
+    string = '))))'
+    print(bracket_match(string) == 4)
+    string = ')('
+    print(bracket_match(string) == 2)
+    string = '()()()()()'
+    print(bracket_match(string) == 0)
+
+print('mismatch count:')
+tests()
+
+# using only Os(1)
+def bracket_match(string):
+    counterO = 0
+    counterC = 0 
+    for i in range(0,len(string)):
+        if string[i] == '(':
+            counterO +=1
+        elif counterO > 0: 
+            counterO -= 1
+        else:
+            counterC += 1
+    return counterO+counterC
+
+print('Os(1)')
+tests()
+
+
+
+
+
+
