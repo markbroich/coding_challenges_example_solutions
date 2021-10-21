167c478f4cf0222654cd82082d336ed79c19a449
mark.broich@gmail.com <mark.broich@gmail.com>
2021-10-21 17:31:58 -0400

    bfs shortest path version where I add path_len to queue as tup of node and path_len

--- coding_challenges_example_solutions/shortest_word_edit_path/shortest_word_edit_path.py
+++ coding_challenges_example_solutions/shortest_word_edit_path/shortest_word_edit_path.py
@@ -161,3 +161,74 @@ def testing():
 testing()
 
 
+# populate transDict (graph of translations)
+# Ot(len(WordShort)*len(WordShort)*len(Source))
+# Os(len(WordShort))
+def pop_graph(words):
+    G = {}
+    def add_to_graph(wa, wb):
+        if wa in G:
+            if wb not in G[wa]:
+                G[wa].append(wb)
+        else:
+            G[wa] = [wb]
+    for i in range(0, len(words)):
+        w1 = words[i]
+        for j in range(0, len(words)):
+            w2 = words[j]
+            if i != j and w1:
+                mc = 0
+                for k in range(0, len(w1)):
+                    if w1[k] == w2[k]:
+                        mc += 1
+                if mc == len(w1) -1:
+                    add_to_graph(w1, w2)
+                    add_to_graph(w2, w1)
+    return G
+
+def shortestWordEditPath(source, target, words):
+    if len(source) != len(target):
+      return -1
+    G = pop_graph(words + [source])    
+
+    # BFS
+    # Ot(len(words)) (visiting each word at most once)
+    # Os(len(words))
+    d = 0
+    Q = [(source, d)]
+    seen = set(source)
+    while Q:
+        cur, d = Q.pop(0)
+        if cur == target:
+            return d
+        for n in G[cur]:
+            if n not in seen:
+                seen.add(n)
+                Q.append((n, d + 1))    
+    return -1
+
+
+
+
+
+
+
+source = "bit"
+target = "dog"
+words = ["but", "put", "big", "pot", "pog", "dog", "lot"]
+exp = 5
+# explanation: bit -> but -> put -> pot -> pog -> dog has 5 transitions.
+print(shortestWordEditPath(source, target, words) == exp)
+
+source = "bit" 
+target = "pog"
+words = ["but","put","big","pot","pog","pig","dog","lot"]
+exp = 3
+print(shortestWordEditPath(source, target, words) == exp)
+
+source = "no"
+target = "go"
+words = ["to"]
+exp = -1
+print(shortestWordEditPath(source, target, words) == exp)
+
