70c69c5ef16ea19c4a8b1144ee5a76275fbe0bce
mark.broich@gmail.com <mark.broich@gmail.com>
2023-01-17 16:02:39 -0500

    added more comapact code version to phone_no_to_letter_str

--- coding_challenges_example_solutions/phone_no_to_letter_str/phone_no_to_letter_str.py
+++ coding_challenges_example_solutions/phone_no_to_letter_str/phone_no_to_letter_str.py
@@ -40,7 +40,26 @@ def phone_no_to_letter_str(number: str, lookup: dict) -> list:
     return rec(0, '')
 
 
+# same compelxity
+def phone_no_to_letter_str_concise(number: str, lookup: dict) -> list:
+    if not number:
+        return []
+    res = []
+
+    def rec(i: int, letters: str) -> None:
+        if i == len(number):
+            res.append(letters)
+        else:
+            if number[i] == '1':
+                i += 1
+            [rec(i + 1, letters + opt) for opt in lookup[number[i]]]
+
+    rec(0, '')
+    return res
+
+
 lookup = {'2': 'ABC', '3': 'D#F', '4': 'GHI', '5': 'JKL', '6': 'MNO', '7': 'PQRS', '8': 'TUV', '9': 'WXYZ'}
 number = "56"
 exp = ["JM","JN","JO","KM","KN","KO","LM","LN","LO"]
 print(phone_no_to_letter_str(number, lookup) == exp)
+print(phone_no_to_letter_str_concise(number, lookup) == exp)
