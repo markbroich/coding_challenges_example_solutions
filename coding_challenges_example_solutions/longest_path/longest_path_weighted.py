de451e174e17779d427ba3d6593c7a9226ca90ee
mark.broich@gmail.com <mark.broich@gmail.com>
2023-02-28 15:23:23 -0500

    finished Dijkstra to longest_path

--- coding_challenges_example_solutions/longest_path/longest_path_weighted.py
+++ coding_challenges_example_solutions/longest_path/longest_path_weighted.py
@@ -1,12 +1,21 @@
 '''
-Find the shortest and longest path through an weighted graph
+Find the shortest and longest path through an weighted, directed graph
 
-Exampel 1
-from_node: (to_node, distance)
+Exampel:
+graph = {from_node: (to_node, distance)}
+
 start = 1
-end = 4
-graph1 = {1:[(2,1)], 2:[(1,1),(3,1),(4,3)], 3:[(2,1),(4,1)], 4:[(3,1),(2,3)]}
-shotrest1 = 3
+end = 5
+graph = {1:[(2,7),(3,9),(6,14)],
+            2:[(4,15)],
+            3:[(2,10),(4,15),(6,2)],
+            4:[(5,6)],
+            5:[],
+            6:[(5,9)]
+            }
+
+shortest = 20
+longest = 30
 '''
 
 
@@ -28,7 +37,7 @@ def dykstra_shortest(graph, start, end):
     return min_so_far[end]
 
 
-# inefficient!
+# inefficient! Using a heap would help
 def find_next_smallest_node(dist_so_far, complete):
     value=float('inf')
     node = None
@@ -40,6 +49,26 @@ def find_next_smallest_node(dist_so_far, complete):
     return node
 
 
+def dykstra_longest(graph, start, end):
+    # init
+    dist_so_far = {k: float('-inf') for k in graph}
+    dist_so_far[start] = 0
+    if start not in dist_so_far or end not in dist_so_far:
+        return -1
+    node = start
+
+    complete = set()
+    while node:
+        complete.add(node)
+        for k, d in graph[node]:
+            if k not in complete:
+                dist_so_far[k] = max(dist_so_far[k], dist_so_far[node] + d)
+        node = find_next_largest_node(dist_so_far, complete, end)
+        # print(node)
+    return dist_so_far[end]
+
+
+# inefficient! Using a heap would help
 def find_next_largest_node(dist_so_far, complete, end):
     value=float('-inf')
     node = None
@@ -52,32 +81,42 @@ def find_next_largest_node(dist_so_far, complete, end):
 
 
 def tests():
+    # Directed graph
+    start = 1
+    end = 5
+    graph3 = {1:[(2,7),(3,9),(6,14)],
+              2:[(4,15)],
+              3:[(2,10),(4,15),(6,2)],
+              4:[(5,6)],
+              5:[],
+              6:[(5,9)]
+              }
+
+    shortest3 = 20
+    longest3 = 30
+    print('Directed graph:')
+    print(dykstra_shortest(graph3, start, end) == shortest3)
+    print(dykstra_longest(graph3, start, end) == longest3)
+
+
+    # undirected graphs work for dykstra_shortest only.
     start = 1
     end = 4
     graph1 = {1:[(2,1)], 2:[(1,1),(3,1),(4,3)], 3:[(2,1),(4,1)], 4:[(3,1),(2,3)]}
-    shotrest1 = 3
-    print(dykstra_shortest(graph1, start, end) == shotrest1)
+    shortest1 = 3
+    print(dykstra_shortest(graph1, start, end) == shortest1)
 
-    start = 1
-    end = 4
     graph2 = {1:[(2,1)], 2:[(1,1),(3,1)], 3:[(2,1)]}
-    shotrest2 = -1
-    print(dykstra_shortest(graph2, start, end) == shotrest2)
+    shortest2 = -1
+    print(dykstra_shortest(graph2, start, end) == shortest2)
 
-    #
-    start = 1
-    end = 4
     graph1 = {1:[(2,1)], 2:[(1,1),(3,1),(4,3)], 3:[(2,1),(4,1)], 4:[(3,1),(2,3)]}
-    shotrest1 = 3
-    print(dykstra_shortest(graph1, start, end) == shotrest1)
+    shortest1 = 3
+    print(dykstra_shortest(graph1, start, end) == shortest1)
 
-    start = 1
-    end = 4
     graph2 = {1:[(2,1)], 2:[(1,1),(3,1)], 3:[(2,1)]}
-    shotrest2 = -1
-    print(dykstra_shortest(graph2, start, end) == shotrest2)
+    shortest2 = -1
+    print(dykstra_shortest(graph2, start, end) == shortest2)
 
-
-    # longest: TBD
 
 tests()
