2fd28c78260239a073f7f17b9b468b3eecb0928c
mark.broich@gmail.com <mark.broich@gmail.com>
2023-05-07 17:12:47 -0400

    unscrambel_medical_terms cleaned up :)

--- coding_challenges_example_solutions/medical_terms/unscrambel_medical_terms.py
+++ coding_challenges_example_solutions/medical_terms/unscrambel_medical_terms.py
@@ -1,11 +1,12 @@
-'''Use all peices to from a medical term'''
+'''Use all pieces to from a medical term'''
 
-pieces = ['he', 'ta', 'Co', 'iv', 
+pieces = ['he', 'ta', 'Co', 'iv',
           'l', 're', 'e', 'bo',
           'mp', 'ns', 'Pa', 'ne',
           'li', 'Me', 'c'
           ]
-words = ['nurse', 'sample', 'hosptial', 'metabolic', 'blood', 'comprehensive', 'panel']
+words = ['nurse', 'sample', 'hosptial', 'metabolic',
+         'blood', 'comprehensive', 'panel']
 
 
 # Does not yet return 'comprehsive metabolic panel' in the correct order
@@ -13,6 +14,7 @@ words = ['nurse', 'sample', 'hosptial', 'metabolic', 'blood', 'comprehensive', '
 # It does however return the comprehensive, panel, metabolic so I got the
 # words :).
 def unscrambel_pieces_to_medical_phrase(pieces: list, words: list) -> str:
+    '''dfs trial and error recursion'''
     target_length = sum([len(piece) for piece in pieces])
     seen = set()
     res = []
@@ -25,63 +27,31 @@ def unscrambel_pieces_to_medical_phrase(pieces: list, words: list) -> str:
             if piece in seen:
                 continue
             for word in words:
-                rec = False
-                if new_word and piece == word[0:len(piece)]:
-                    res.append(piece)
-                    new_word = False
-                    rec = True
-                elif res:
-                    part = res[-1] + piece
-                    if part == word:
-                        res[-1] = part
-                        new_word = True
-                        rec = True
-                    elif part == word[0:len(part)]:
-                        res[-1] = part
-                        rec = True
-                if rec:
+                if can_append_piece(res, word, new_word, piece):
                     seen.add(piece)
                     lenght += len(piece)
                     return recursion(new_word, lenght)
         return False
 
     if recursion(True, 0):
-        return res
+        return ' '.join(res)
     return ''
 
 
-print(unscrambel_pieces_to_medical_phrase(pieces, words))
-
-# def unscrambel_pieces_to_medical_phrase(pieces: list, words: list) -> str:
-#     seen = set()
-#     res = []
-#     def recursion(new_word: bool):
-#         for i in range(0, len(pieces)):
-#             piece = pieces[i].lower()
-#             if piece in seen:
-#                 continue
-#             for word in words:
-#                 rec = False
-#                 if new_word and piece == word[0:len(piece)]:
-#                     res.append(piece)
-#                     new_word = False
-#                     rec = True
-#                 elif res:
-#                     part = res[-1] + piece
-#                     if part == word:
-#                         res[-1] = part
-#                         new_word = True
-#                         rec = True
-#                     elif part == word[0:len(part)]:
-#                         res[-1] = part
-#                         rec = True
-#                 if rec:
-#                     seen.add(piece)
-#                     return recursion(new_word)
-#         return
-
-#     recursion(True)
-#     return res
+def can_append_piece(res: list, word: str, new_word: bool, piece: str) -> bool:
+    if new_word and piece == word[0:len(piece)]:
+        res.append(piece)
+        new_word = False
+        return True
+    elif res:
+        part = res[-1] + piece
+        if part == word:
+            res[-1] = part
+            new_word = True
+            return True
+        elif part == word[0:len(part)]:
+            res[-1] = part
+            return True
 
 
-# print(unscrambel_pieces_to_medical_phrase(pieces, words))
+print(f'reconstructed term: {unscrambel_pieces_to_medical_phrase(pieces, words)}')
